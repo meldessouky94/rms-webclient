f38f04dc58aa339d28cf1813c3308c65eb1d502f
thecalebwoods <thecalebwoods@gmail.com>
2018-12-06 20:44:10 -0500

    new reservation

--- src/app/components/shared/resource-form/resource-form.component.ts
+++ src/app/components/shared/resource-form/resource-form.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ResourceService } from 'src/app/services/resource/resource.service';
+import { ReservationService } from 'src/app/services/reservation/reservation.service';
 
 @Component({
   selector: 'app-resource-form',
   templateUrl: './resource-form.component.html',
-  styleUrls: ['./resource-form.component.sass']
+  styleUrls: ['./resource-form.component.css']
 })
 export class ResourceFormComponent implements OnInit {
+campus;
+building;
+purpose;
+date;
+time1;
+time2;
+email;
 
-  constructor() { }
+  constructor(private resourceServ: ResourceService, private resServ: ReservationService) { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
 
+}
+
+submit() {
+
+const start = this.date + ' ' + this.time1 + ':000';
+const end = this.date + ' ' + this.time2 + ':000';
+
+const reservation = {
+'buildingId': 1,
+'start': start,
+'end': end,
+'resourceId': 0,
+'resource': null,
+'userEmail': this.email,
+'purpose': this.purpose,
+'cancelled': false,
+'approved': true,
+};
+
+this.resServ.queryAvailableResources(reservation);
+this.resServ.reservation = reservation;
+
+console.log(reservation);
+}
 }
--- src/app/services/reservation/reservation.service.ts
+++ src/app/services/reservation/reservation.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  reservation;
+  constructor(private httpClient: HttpClient) { }
+
+  queryAvailableResources(resObject) {
+    // const url = `http...`
+   // return this.httpClient.get(url, resObject);
+  }
+
+  queryNewReservation(id , resource) {
+    this.reservation.id = id;
+    this.reservation.resource = resource;
+    // const url = `http...`
+    // return this.httpclient.post(url, this.reservation)
+  }
 }
