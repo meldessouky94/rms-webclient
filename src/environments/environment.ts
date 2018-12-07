54adc251fd84ab9700868add333539dfa7fef5ab
DLedger <DLedger@fullsail.edu>
2018-12-07 11:14:59 -0500

    URL environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  localUrl: "http://localhost:8080/",
+  ec2Url: ""
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
