dd8a6c0851e2cfa994f9c2d9faf611d827caab82
Clay <randyclayandrews@gmail.com>
2018-12-08 14:07:08 -0500

    Added resource service

--- src/app/services/resource/resource.service.ts
+++ src/app/services/resource/resource.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourceService {
 
-  constructor() { }
+  apiUrl = `${environment.apiUrl}resources`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  // Returns an array of campuse objects. Each campus object contains a list of building objects.
+  getCampuses() {
+    let url = this.apiUrl;
+    url += '/campuses';
+    return this.httpClient.get(url, {withCredentials: true});
+  }
 }
