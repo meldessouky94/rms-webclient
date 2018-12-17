6f57416d58c9ef010398cacc35ef9d688a997e67
Mitchell Goshorn <msgoshorn@gmail.com>
2018-12-17 10:58:34 -0500

    Removed environment variable reference as this won't work on an Angular app. Once our static webhost is configured, this needs to be updated in the cloud-dev environment to point to the appropriate URL

--- src/environments/environment.cloud-dev.ts
+++ src/environments/environment.cloud-dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://sonarcloud.io/organizations/1809-spark-usf/projects/',
-    appUrl: process.env.RMS_APP_URL || 'localhost:4200',
+    appUrl: 'localhost:4200', // TODO update once static site is configured
     serviceContext: {
         resource: 'rms-resource',
         reservation: 'rms-reservation'
