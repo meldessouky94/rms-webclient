68dac6eaba6d2041deface6a94a1a06621b56d74
Clay Andrews <rclayandrews@outlook.com>
2018-12-17 10:16:18 -0500

    Passes tests, added env. var. for slack client ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080/',
-  appUrl: 'S3 URL here'
+  appUrl: 'S3 URL here',
+  slackClientId: '432094667025.496759859637'
 };
