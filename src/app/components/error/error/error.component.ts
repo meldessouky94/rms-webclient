ea3be258c72c8480271a689cb4a42e89990cc801
madisonbynum <madison.a.bynum@gmail.com>
2018-12-17 16:16:54 -0500

    styling, making everything orange, and spaced right

--- src/app/components/error/error/error.component.ts
+++ src/app/components/error/error/error.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.sass']
+  styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
