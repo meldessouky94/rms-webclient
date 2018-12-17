b3f7ef2ea8015aa0f4a0ac689bb4ff2e5b1fc971
Mitchell Goshorn <msgoshorn@gmail.com>
2018-12-17 09:57:03 -0500

    Added injectable value for S3 application URL

--- src/environments/environment.cloud-dev.ts
+++ src/environments/environment.cloud-dev.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://sonarcloud.io/organizations/1809-spark-usf/projects/',
+    appUrl: process.env.RMS_APP_URL || 'localhost:4200',
     serviceContext: {
         resource: 'rms-resource',
         reservation: 'rms-reservation'
