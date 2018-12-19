ba15b526f24dd069bdc04100a95c560ccee0982c
Mitchell Goshorn <msgoshorn@gmail.com>
2018-12-19 10:53:30 -0500

    Fixed broken environment link

--- src/environments/environment.cloud-dev.ts
+++ src/environments/environment.cloud-dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://sonarcloud.io/organizations/1809-spark-usf/projects/',
+    apiUrl: 'http://mitchell-rms-1500652919.us-west-1.elb.amazonaws.com/',
     appUrl: 'localhost:4200', // TODO update once static site is configured
     serviceContext: {
         resource: 'rms-resource',
