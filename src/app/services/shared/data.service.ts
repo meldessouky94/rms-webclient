19bc40040f8fd2b74d10ebf806618d9e7e35bcee
David L. Batton <battondl@gmail.com>
2019-01-04 10:48:46 -0500

    Added DataService spec file

--- src/app/services/shared/data.service.ts
+++ src/app/services/shared/data.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-@Injectable()
+/**
+ * Injectable
+ */
+@Injectable({
+  providedIn: 'root'
+})
 export class DataService {
 
   private messageSource = new BehaviorSubject(false);
