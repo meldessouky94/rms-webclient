c07f3aea2bd4c3eeac2841aa978144dea5ace53e
ThiagoM728 <tcm728@gmail.com>
2019-03-19 11:11:40 -0400

    created tests for UserService.getUserById()

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -8,21 +8,28 @@ import { User } from '../../models/user';
 describe('UserService', () => {
   let service: UserService
   let httpMock: HttpTestingController
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-      ],
+      imports: [HttpClientModule],
+      providers: [UserService],
     })
     service = TestBed.get(UserService)
     httpMock = TestBed.get(HttpTestingController)
   });
 
+  /**
+   * Generated Test
+   * Makes sure that the service is initialized by Angular
+   */
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  /**
+   * nextCurrentUser Test
+   * Tests that is authenticated, currentUser, and $currentUser are all properly set when user is
+   * passed in as a truthy value.
+   */
   it('#nextCurrentUser should set the current user if the passed in user is truthy', () => {
     let user: User = new User();
     service.nextCurrentUser(user)
@@ -34,15 +41,34 @@ describe('UserService', () => {
     expect(service.currentUser).toBeTruthy()
   })
 
+  /**
+   * nextCurrentUser Test
+   * Tests that is authenticated, currentUser, and $currentUser are all properly set when user is
+   * passed in as a falsy value.
+   */
   it('#nextCurrentUser should set isAuthenticated to be falsy if user passed in is falsy', () => {
     service.nextCurrentUser(undefined)
-    let testSub = service.$currentUser.subscribe(u => {
+    const testSub = service.$currentUser.subscribe((u) => {
       expect(u).toBeFalsy()
-      testSub.unsubscribe()
+      testSub.unsubscribe();
     })
     expect(service.isAuthenticated).toBeFalsy()
-    expect(service.currentUser).toBeFalsy()
+    expect(service.currentUser).toBeFalsy();
   })
 
+  /**
+   * getUserById Test
+   * Tests that getUserId sends a get request and receives a truthy user object
+   */
+  it('#getUserById should send a get request with the user id ', () => {
+    const expectedUser: User = new User();
+    const targetId = '1';
+    service.getUserById(targetId).subscribe( (user) => {
+      expect(user).toBeTruthy();
+    });
+    const request = httpMock.expectOne(`${service.apiUrl}/users/${targetId}`);
+    expect(request.request.method).toBe('GET');
 
+    request.flush(expectedUser);
+  });
 });
