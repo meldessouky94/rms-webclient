1a686da333b2130a4e1baf408955fdc7bad1b886
Mohamed Eldessouky <meldessouky94@gmail.com>
2019-03-19 15:29:56 -0400

    Finished all resource testing in karma

--- src/app/services/resource/resource.service.spec.ts
+++ src/app/services/resource/resource.service.spec.ts
@@ -4,50 +4,81 @@ import { environment } from '../../../environments/environment';
 
 import { ResourceService } from './resource.service';
 import { async } from 'q';
+import { SearchDto } from 'src/app/models/search-dto';
+import { NgbModalWindow } from '@ng-bootstrap/ng-bootstrap/modal/modal-window';
+import { Resource } from 'src/app/models/resource';
+import { Observable } from 'rxjs';
+import { resource } from 'selenium-webdriver/http';
 
 
 
 describe('ResourceService', () => {
-  let service: ResourceService
-  let httpMock : HttpTestingController
-
+  let httpClientSpy: { get: jasmine.Spy }
+  let resourceService: ResourceService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [ResourceService]
-    });
-    service = TestBed.get(ResourceService)
-    httpMock = TestBed.get(HttpTestingController)
-
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    resourceService = new ResourceService(<any>httpClientSpy)
   });
 
-  afterEach(() => {
-    httpMock.verify();
+  it('should be created ', () => {
+    expect(resourceService).toBeTruthy();
+  })
+
+  describe('pushNewCurrentResourceList', () => {
+    it('Should push an array of resources to the current resource list', (done: DoneFn) => {
+      const dummyResource: Resource[] = [new Resource()];
+      resourceService.$currentResourceList.subscribe(resourceList => {
+        expect(resourceList).toBe(dummyResource);
+        done();
+      });
+      resourceService.pushNewCurrentResourceList(dummyResource);
+      expect(resourceService.currentResourceList).toBe(dummyResource)
+    })
+    
+    it('Should push a false array to the current resource list unsuccessfully', (done: DoneFn) => {
+      const testSub = resourceService.$currentResourceList.subscribe((u) => {
+        expect(u).toBeFalsy();
+        testSub.unsubscribe();
+        done();
+      })
+      resourceService.pushNewCurrentResourceList(undefined);
+      expect(resourceService.currentResourceList).toBeFalsy();
+    })
+  })
+
+  describe('getCampuses', () => {
+    it('Should send a get request and return an observable', () => {
+      httpClientSpy.get.and.returnValue(new Observable<any>());
+      resourceService.getCampuses().subscribe();
+      expect(httpClientSpy.get.calls.count()).toBe(1);
+    })
   });
 
-  fit('should retrieve campuses from the API via GET', () => {
-    const dummyCampuses: any[] = [ {
-      id: 1,
-      name: 'USF',
-      buildings: [{id: 1, name: 'Building 1', campus_id: 1}, {id: 2, name: 'Building 2', campus_id: 1} ]
-    }, {
-      id: 2,
-      name: 'MoCo',
-      buildings: [{id: 3, name: 'Building 5', campus_id: 2}, {id: 4, name: 'Building 6', campus_id: 2} ]
-    }];
-    service.getCampuses().subscribe(campuses => {
-      expect(campuses.length).toBe(2);
-      expect(campuses).toEqual(dummyCampuses);
-    });
+  describe('getResourceById', () => {
+    it('#getResourceById should send a get request with the resource id', () => {
+      const targetId = 1;
+      httpClientSpy.get.and.returnValue(new Observable<Resource>());
+      resourceService.getResourceById(targetId).subscribe();
+      expect(httpClientSpy.get.calls.count()).toBe(1);
+    })
+  })
 
-    const request = httpMock.expectOne(`${service.apiUrl}${environment.serviceContext.resource}/campuses`);
-    expect(request.request.method).toBe('GET');
-
-    request.flush(dummyCampuses);
-  });
-  it('should be created', () => {
-    const service: ResourceService = TestBed.get(ResourceService);
-    expect(service).toBeTruthy();
-  });
+  describe('getAvailableResources', () => {
+    it('Should send a get request with the searchDTO', () => {
+      const dummySearch: SearchDto = {
+        buildingId: 1,
+        startTime: '1',
+        endTime: '2',
+        purpose: 'study',
+        campusId: 1,
+        reminderTime: 30
+      };
+  
+      httpClientSpy.get.and.returnValue(new Observable<Resource[]>());
+      resourceService.getAvailableResources(dummySearch).subscribe();
+      expect(httpClientSpy.get.calls.count()).toBe(1);
+    })
+  })
 });
+
