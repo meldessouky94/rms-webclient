2823f9255caff714f7a7cf7d305734edfce2b25b
Jose Meono <jose.a.meono@gmail.com>
2019-03-20 11:26:54 -0400

    implemented testing for admin folder services

--- src/app/services/admin/admin-login.service.spec.ts
+++ src/app/services/admin/admin-login.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AdminLoginService } from './admin-login.service';
+import { Admin } from 'src/app/models/admin';
+import { Observable } from 'rxjs';
 
 describe('AdminLoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let adminLoginService: AdminLoginService;
+  let httpClientSpy: {post: jasmine.Spy};
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    adminLoginService = new AdminLoginService(<any> httpClientSpy);
+  });
 
   it('should be created', () => {
-    const service: AdminLoginService = TestBed.get(AdminLoginService);
-    expect(service).toBeTruthy();
+    expect(adminLoginService).toBeTruthy();
+  });
+  it('should make a post request to validateUser', () => {
+    const dummyAdmin: Admin = new Admin();
+
+    httpClientSpy.post.and.returnValue(new Observable<Admin>());
+    adminLoginService.validateUser(dummyAdmin).subscribe();
+    expect(httpClientSpy.post.calls.count()).toBe(1);
   });
 });
