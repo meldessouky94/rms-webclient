fd6b22d9e5d703daf0d90b7d48bd57f5e6940351
Jose Meono <jose.a.meono@gmail.com>
2019-03-20 10:40:47 -0400

    implemented testing for services under the shared folder

--- src/app/services/shared/is-admin-behavior-set.service.spec.ts
+++ src/app/services/shared/is-admin-behavior-set.service.spec.ts
@@ -3,10 +3,25 @@ import { TestBed } from '@angular/core/testing';
 import { IsAdminBehaviorSetService } from './is-admin-behavior-set.service';
 
 describe('IsAdminBehaviorSetService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let abehaviorService: IsAdminBehaviorSetService;
+  beforeEach(() => {
+    abehaviorService = new IsAdminBehaviorSetService();
+  });
 
   it('should be created', () => {
-    const service: IsAdminBehaviorSetService = TestBed.get(IsAdminBehaviorSetService);
-    expect(service).toBeTruthy();
+    expect(abehaviorService).toBeTruthy();
+  });
+  it('should receive the next value false', (done: DoneFn) => {
+    abehaviorService.currentMessage.subscribe((value) => {
+      expect(value).toBeFalsy();
+      done();
+    });
+  });
+  it('should receive the next value true', (done: DoneFn) => {
+    abehaviorService.changeBoolean(true);
+    abehaviorService.currentMessage.subscribe((value) => {
+      expect(value).toBeTruthy();
+      done();
+    });
   });
 });
