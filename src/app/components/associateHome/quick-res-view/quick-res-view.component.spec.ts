a34c9b68a40cba8995c9178f4f5b88cabf13ca15
ThiagoM728 <tcm728@gmail.com>
2019-03-25 15:56:09 -0400

    Finished quick-res-view.component testing

--- src/app/components/associateHome/quick-res-view/quick-res-view.component.spec.ts
+++ src/app/components/associateHome/quick-res-view/quick-res-view.component.spec.ts
@@ -58,35 +58,3 @@ describe('QuickResViewComponent', () =>{
   })
 
 });
-
-// describe('QuickResViewComponent', () => {
-//   let component: QuickResViewComponent;
-//   let fixture: ComponentFixture<QuickResViewComponent>;
-//   let testBedService: ReservationService;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [QuickResViewComponent],
-//       providers: [ReservationService],
-//       imports: [HttpClientModule],
-//     })
-//       .compileComponents();
-//   }));
-//
-//   beforeEach(async () => {
-//     fixture = TestBed.createComponent(QuickResViewComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     testBedService = TestBed.get(ReservationService);
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-//
-//   it('Service injected and testBed Service are same instance',
-//     inject([ReservationService], (injectedService: ReservationService) => {
-//       expect(injectedService).toBe(testBedService);
-//     }),
-//   );
-// });
