a5cd3854b80e73a1fdafa5a561f6718c317bd868
Tyler Williams <tw1235@msstate.edu>
2019-03-25 17:08:13 -0400

    wrote component tests for admin-long page

--- src/app/components/admin/admin-edit-reservation/admin-edit-reservation/admin-edit-reservation.component.spec.ts
+++ src/app/components/admin/admin-edit-reservation/admin-edit-reservation/admin-edit-reservation.component.spec.ts
@@ -1,25 +1,124 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ResourceService } from './../../../../services/resource/resource.service';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { AdminEditReservationComponent } from './admin-edit-reservation.component';
+import { AdminEditReservationComponent } from './admin-edit-reservation.component';
+import { Subject } from 'rxjs';
+import { Reservation } from 'src/app/models/reservation';
+import { Resource } from 'src/app/models/resource';
+import { User } from 'src/app/models/user';
+import { BehaviorSubject } from 'rxjs';
 
-// describe('AdminEditReservationComponent', () => {
-//   let component: AdminEditReservationComponent;
-//   let fixture: ComponentFixture<AdminEditReservationComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AdminEditReservationComponent ]
-//     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AdminEditReservationComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+/**
+ * Admin Edit Reservation Component Unit Tests
+ * @author Tyler Williams
+ */
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('AdminEditReservationComponent', () => {
+    let component: AdminEditReservationComponent;
+    let fixture: ComponentFixture<AdminEditReservationComponent>;
+
+    // Mocking component dependencies
+    let reservationIdBehaviorSetServiceStub: {
+        currentMessage: Subject<number>
+    } = {
+        currentMessage: new Subject()
+    }
+    let reservationServiceStub: {
+        currentReservation: Reservation,
+        $currentReservation: Subject<Reservation>,
+        userReservations: Reservation[],
+        $userReservations: Subject<Reservation[]>,
+    } = {
+        currentReservation: undefined,
+        $currentReservation: new Subject(),
+        userReservations: undefined,
+        $userReservations: new Subject()
+    };
+    let resourceServiceStub: {
+        currentResourceList: Resource[],
+        $currentResourceList: Subject<Resource[]>,
+        getResourceById: jasmine.Spy
+    } = {
+        currentResourceList: undefined,
+        $currentResourceList: new Subject(),
+        getResourceById: undefined
+    };
+    let userServiceStub: {
+        currentUser: User,
+        $currentUser: Subject<User>
+    } = {
+        currentUser: undefined,
+        $currentUser: new Subject()
+    };
+
+
+    //   beforeEach(async(() => {
+    //     TestBed.configureTestingModule({
+    //       declarations: [ AdminEditReservationComponent ]
+    //     })
+    //     .compileComponents();
+    //   }));
+
+    //   beforeEach(() => {
+    //     fixture = TestBed.createComponent(AdminEditReservationComponent);
+    //     component = fixture.componentInstance;
+    //     fixture.detectChanges();
+    //   });
+
+    it('should create', () => {
+        component = new AdminEditReservationComponent(
+            <any>reservationIdBehaviorSetServiceStub,
+            <any>reservationServiceStub,
+            <any>resourceServiceStub,
+            <any>userServiceStub
+        );
+        expect(component).toBeTruthy();
+    });
+
+    it('should set the current message upon component init', () => {
+        component = new AdminEditReservationComponent(
+            <any>reservationIdBehaviorSetServiceStub,
+            <any>reservationServiceStub,
+            <any>resourceServiceStub,
+            <any>userServiceStub
+        );
+        component.ngOnInit();
+        expect(reservationIdBehaviorSetServiceStub.currentMessage).toBeTruthy();
+    });
+
+
+    // describe('setReservationId', () => {
+    //     it('should set the reservation id', () => {
+
+    //     });
+    // });
+
+    // describe('findReservationById', () => {
+    //     it('should find the reservation by id', () => {
+
+    //     });
+    // });
+
+    // describe('findResourceById', () => {
+    //     it('should find the resource by id', () => {
+    //         component = new AdminEditReservationComponent(
+    //             <any>reservationIdBehaviorSetServiceStub,
+    //             <any>reservationServiceStub,
+    //             <any>resourceServiceStub,
+    //             <any>userServiceStub
+    //         );
+    //         resourceServiceStub.getResourceById = spyOn(ResourceService.prototype, 'getResourceById');
+    //         // expect(resourceServiceStub.getResourceById).toHaveBeenCalled();
+    //         resourceServiceStub
+
+    //   });
+    // });
+
+    // describe('findUserById', () => {
+    //     it('should find a user by id', () => {
+
+    //     });
+    // });
+});
--- src/app/components/admin/admin-login/admin-login.component.spec.ts
+++ src/app/components/admin/admin-login/admin-login.component.spec.ts
@@ -1,25 +1,118 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { AdminLoginComponent } from './admin-login.component';
+import { AdminLoginComponent } from './admin-login.component';
+import { Subject } from 'rxjs';
+import { Admin } from 'src/app/models/admin';
+import { AdminLoginService } from 'src/app/services/admin/admin-login.service';
 
-// describe('AdminLoginComponent', () => {
-//   let component: AdminLoginComponent;
-//   let fixture: ComponentFixture<AdminLoginComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AdminLoginComponent ],
-//     })
-//     .compileComponents();
-//   }));
+/**
+ * Admin Login Component Unit Tests
+ * @author Tyler Williams
+ */
+describe('AdminLoginComponent', () => {
+    let component: AdminLoginComponent;
+    let fixture: ComponentFixture<AdminLoginComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AdminLoginComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    // Mock dependencies for the component
+    let adminLoginServiceStub: {
+        validateUser: jasmine.Spy
+    } = jasmine.createSpyObj('AdminLoginService', ['validateUser']);
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    let isAdminBehaviorSetServiceStub: {
+        currentMessage: Subject<number>
+    } = {
+        currentMessage: new Subject()
+    }
+
+    let titleBehaviorSetServiceStub: {
+        currentMessage: Subject<string>
+    } = {
+        currentMessage: new Subject()
+    }
+    
+
+
+    it('should create', () => {
+        component = new AdminLoginComponent(
+            <any> adminLoginServiceStub,
+            <any> isAdminBehaviorSetServiceStub,
+            <any> titleBehaviorSetServiceStub
+        );
+        expect(component).toBeTruthy();
+    });
+
+    it('should set justRegistered to true on init'
+    + ' if the admin has just registered', () => {
+        component = new AdminLoginComponent(
+            <any> adminLoginServiceStub,
+            <any> isAdminBehaviorSetServiceStub,
+            <any> titleBehaviorSetServiceStub
+        );
+        let getItem = spyOn(sessionStorage, 'getItem');
+        let setItem = spyOn(sessionStorage, 'setItem');
+
+        getItem.and.returnValue('yes');
+        component.ngOnInit();
+
+        expect(component.justRegistered).toBeTruthy();
+        expect(setItem).toHaveBeenCalledWith('justRegistered', 'no');
+    });
+
+    it('should set justRegistered to false on init'
+    + ' if the admin has just registered', () => {
+        component = new AdminLoginComponent(
+            <any> adminLoginServiceStub,
+            <any> isAdminBehaviorSetServiceStub,
+            <any> titleBehaviorSetServiceStub
+        );
+        let getItem = spyOn(sessionStorage, 'getItem');
+        let setItem = spyOn(sessionStorage, 'setItem');
+
+        getItem.and.returnValue('');
+        component.ngOnInit();
+
+        expect(component.justRegistered).toBeFalsy();
+        expect(setItem).toHaveBeenCalledWith('justRegistered', 'no');
+    });
+
+    describe('onSubmitClick', () => {
+        it('should call runLoginEvents if admin object'
+        + ' is successfully returned by the server', () => {
+            component = new AdminLoginComponent(
+                <any> adminLoginServiceStub,
+                <any> isAdminBehaviorSetServiceStub,
+                <any> titleBehaviorSetServiceStub
+            );
+
+            let runLogin = spyOn(AdminLoginComponent.prototype, 'runLoginEvents');
+            let fakeSubject = new Subject<Admin>(); // makes container to put admin "reponse" in
+
+            adminLoginServiceStub.validateUser.and.returnValue(fakeSubject);
+            component.onSubmitClick();
+            fakeSubject.next(component.admin); // fills fakeSubject container with component.admin
+            expect(runLogin).toHaveBeenCalled();
+        });
+
+        it('should produce an error if an'
+        + ' error happens in the server', () => {
+            component = new AdminLoginComponent(
+                <any> adminLoginServiceStub,
+                <any> isAdminBehaviorSetServiceStub,
+                <any> titleBehaviorSetServiceStub
+            );
+            
+            let fakeSubject = new Subject<Admin>();
+            
+            adminLoginServiceStub.validateUser.and.returnValue(fakeSubject);
+            component.onSubmitClick();
+            fakeSubject.error(new Error());
+            expect(component.errorMessage).toBeDefined();
+        });
+
+    });
+
+    describe('runLoginEvents', () => {
+
+    });
+});
