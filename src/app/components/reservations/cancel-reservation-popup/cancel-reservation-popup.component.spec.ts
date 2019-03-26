cd5b64db88309674596512e41769b4d3996b7efb
Olabode Opapeju <olaboe00@gmail.com>
2019-03-25 21:34:30 -0400

    completed CancelResComponent unit testing

--- src/app/components/reservations/cancel-reservation-popup/cancel-reservation-popup.component.spec.ts
+++ src/app/components/reservations/cancel-reservation-popup/cancel-reservation-popup.component.spec.ts
@@ -1,64 +1,102 @@
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
-
 import { CancelReservationPopupComponent } from './cancel-reservation-popup.component';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ReservationService } from 'src/app/services/reservation/reservation.service';
-import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/models/user';
 import { Reservation } from 'src/app/models/reservation';
-import { Subject, Observable } from 'rxjs';
-import { Resource } from 'src/app/models/resource';
-import { SearchDto } from 'src/app/models/search-dto';
+import { Subject } from 'rxjs';
 
+
+/**
+ * CancelReservationComponent unit testing
+ * @author Jose Meono
+ */
 describe('CancelReservationPopupComponent', () => {
     let component: CancelReservationPopupComponent;
 
-    let userServiceStub: { currentUser: User } = {
-        currentUser: undefined,
-    };
-    let ngbModalStub: {}
+    // Declare stubs to mock dependencies
+    let userServiceStub: { currentUser: User };
+    let ngbModalStub: {dismiss: jasmine.Spy};
     let reservationServiceStub: {
-        currentReservation: Reservation, $currenReservation: Subject<Reservation>,
-        userReservations: Reservation[], $userReservations: Subject<Reservation[]>
-    } = {
-        currentReservation: new Reservation(),
-        $currenReservation: new Subject(),
-        userReservations: [new Reservation(), new Reservation()],
-        $userReservations: new Subject(),
+        cancelReservations: jasmine.Spy,
+        getUserReservations: jasmine.Spy,
+        pushNewUserReservations: jasmine.Spy
     };
+
+    // Initialize stubs to default of undefined to reset the function Spys for each test
+    beforeEach(() => {
+        userServiceStub = { currentUser: undefined };
+        ngbModalStub = { dismiss: undefined };
+        reservationServiceStub = {
+            cancelReservations: undefined,
+            getUserReservations: undefined,
+            pushNewUserReservations: undefined
+        }
+    });
+
+    // BEGIN TESTS
     it('should create', () => {
         component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
         expect(component).toBeTruthy();
     });
-    it('should initialize user when component is init', () => {
-        userServiceStub.currentUser = new User();
-        component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
-        expect(component.user).toBe(userServiceStub.currentUser);
-    });
+
     it('should intialize variables during ngOnInit', () => {
+        userServiceStub.currentUser = new User();
         component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
         component.ngOnInit();
         expect(component.resolved).toBeFalsy();
         expect(component.error).toBeFalsy();
+        expect(component.user).toBe(userServiceStub.currentUser);
     });
-    // describe('cancelReservation', () => {
-    //     fit('should cancel the reservation and update', () => {
-    //         const functionStub: {
-    //             cancelReservations: jasmine.Spy,
-    //             getUserReservations: jasmine.Spy,
-    //             pushNewUserReservations: jasmine.Spy,
-    //         } = jasmine.createSpyObj('ReservationService', ['cancelReservations', 'getUserReservation', 'pushNewUserReservations']);
-    //         const dummyCancelSub = new Subject();
-    //         const dummyUserSub = new Subject();
-    //         const dummyListReservation = [new Reservation(), new Reservation()];
-    //         component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>functionStub, <any>userServiceStub);
-    //         console.log(functionStub.cancelReservations);
-    //         functionStub.cancelReservations.and.returnValue(dummyCancelSub);
-    //         functionStub.getUserReservations.and.returnValue(dummyUserSub);
-    //         component.reservation = new Reservation();
-    //         component.cancelReservation();
-    //         expect(functionStub.cancelReservations).toHaveBeenCalled();
-    //     });
-    // });
+
+    it('should unsubscribe from subscriptions used if component is destroyed', () => {
+        const dummyCancelSub = new Subject();
+        const dummyUserSub = new Subject();
+        component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
+        component.cancelResSub = dummyCancelSub.subscribe();
+        component.getUserResSub = dummyUserSub.subscribe();
+
+        component.ngOnDestroy();
+        expect(component.cancelResSub.closed).toBeTruthy();
+        expect(component.getUserResSub.closed).toBeTruthy();
+    });
+
+    describe('cancelReservation', () => {
+        it('should cancel the reservation and update', () => {
+            const dummyCancelSub = new Subject();
+            const dummyUserSub = new Subject();
+            const dummyListReservation = [new Reservation(), new Reservation()];
+            reservationServiceStub.cancelReservations = spyOn(ReservationService.prototype, 'cancelReservations');
+            reservationServiceStub.getUserReservations = spyOn(ReservationService.prototype, 'getUserReservations');
+            reservationServiceStub.pushNewUserReservations = spyOn(ReservationService.prototype, 'pushNewUserReservations');
+            ngbModalStub.dismiss = spyOn(NgbActiveModal.prototype, 'dismiss');
+            
+            reservationServiceStub.cancelReservations.and.returnValue(dummyCancelSub);
+            reservationServiceStub.getUserReservations.and.returnValue(dummyUserSub);
+            component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
+            
+            component.reservation = new Reservation();
+            component.cancelReservation();
+            dummyCancelSub.next();
+            dummyUserSub.next(dummyListReservation);
+
+            expect(reservationServiceStub.pushNewUserReservations).toHaveBeenCalledWith(dummyListReservation);
+            expect(ngbModalStub.dismiss).toHaveBeenCalled()
+        });
+
+        it('should set the error variable to true if an error occurs', () => {
+            const dummyCancelSub = new Subject();
+            reservationServiceStub.cancelReservations = spyOn(ReservationService.prototype, 'cancelReservations');
+            ngbModalStub.dismiss = spyOn(NgbActiveModal.prototype, 'dismiss');
+            reservationServiceStub.cancelReservations.and.returnValue(dummyCancelSub);
+            
+            component = new CancelReservationPopupComponent(<any>ngbModalStub, <any>reservationServiceStub, <any>userServiceStub);
+            component.reservation = new Reservation();
+
+            component.cancelReservation();
+            dummyCancelSub.error(new Error());
+
+            expect(component.error).toBeTruthy();
+            expect(ngbModalStub.dismiss).toHaveBeenCalled();
+        });
+    });
 });
