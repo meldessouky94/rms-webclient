f4148efe6d9c3256eb43d7824779978f3654a908
Tyler Williams <tw1235@msstate.edu>
2019-03-26 13:35:55 -0400

    started writing tests for admin-registration-component

--- src/app/components/admin/admin-registration/admin-registration.component.spec.ts
+++ src/app/components/admin/admin-registration/admin-registration.component.spec.ts
@@ -1,18 +1,53 @@
 import { AdminRegistrationComponent } from "./admin-registration.component";
+import { AdminRegistrationService } from 'src/app/services/admin/admin-registration.service';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Admin } from 'src/app/models/admin';
 
 describe('AdminRegistrationComponent', () => {
   let component: AdminRegistrationComponent;
 
-  let adminRegServiceStub: {};
-  let routerStub: {};
+  let adminRegServiceStub: {
+    registerAdmin: jasmine.Spy
+  };
+
+  let routerStub: {
+    navigate: jasmine.Spy
+  };
 
   beforeEach(() => {
-    adminRegServiceStub = {};
-    routerStub = {};
+    adminRegServiceStub = {
+      registerAdmin: spyOn(AdminRegistrationService.prototype, 'registerAdmin')
+    };
+    routerStub = {
+      navigate: spyOn(Router.prototype, 'navigate')
+    };
   })
 
   it('should create', () => {
     component = new AdminRegistrationComponent(<any>adminRegServiceStub, <any>routerStub);
     expect(component).toBeTruthy();
+  });
+
+  describe('onSubmitClick', () => {
+    it('should call setItem and navigate if admin'
+      + ' registration is successful', () => {
+        component = new AdminRegistrationComponent(
+          <any>adminRegServiceStub,
+          <any>routerStub
+        );
+
+        let setItem = spyOn(sessionStorage, 'setItem');
+        let fakeSubject = new Subject<Admin>(); // makes container to put admin "reponse" in
+        let validInput = spyOn(AdminRegistrationComponent.prototype, 'validInput');
+        let fakeAdmin = new Admin();
+
+        adminRegServiceStub.registerAdmin.and.returnValue(fakeSubject);
+        validInput.and.returnValue(true);
+        component.onSubmitClick();
+        fakeSubject.next(fakeAdmin); // fills fakeSubject container with component.admin
+        expect(setItem).toHaveBeenCalledWith('justRegistered', 'yes');
+        expect(routerStub.navigate).toHaveBeenCalled();
+      });
   });
 });
